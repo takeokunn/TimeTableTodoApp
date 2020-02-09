0770cc19e0d0f88a63522d39c610c86c957db50b
takeokunn <bararararatty@gmail.com>
2020-02-09 23:55:56 +0900

    storyboard追加

---
+++ TimeTableTodoApp/ViewController/Todo/AddViewController.swift
@@ -0,0 +1,15 @@
+import Foundation
+import UIKit
+
+class AddViewController: UITableViewController {
+
+    override func viewDidLoad () {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning () {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
--- TimeTableTodoApp/ViewController/Todo/TodoViewController.swift
+++ TimeTableTodoApp/ViewController/Todo/TodoViewController.swift
@@ -1,9 +1,44 @@
-//
-//  TodoViewController.swift
-//  TimeTableTodoApp
-//
-//  Created by obara_t on 2020/02/09.
-//  Copyright © 2020 obara_t. All rights reserved.
-//
-
 import Foundation
+import UIKit
+
+class TodoViewController: UITableViewController {
+
+    var todos: [String] = ["fdsfs", "fdsafas"]
+
+    override func viewDidLoad () {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning () {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+// MARK: logic
+extension TodoViewController {
+
+    private func initializeTableView() {
+        tableView.rowHeight = 60
+    }
+}
+
+// MARK: tableview
+extension TodoViewController {
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todos.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
+
+        let label1 = cell.viewWithTag(1) as? UILabel
+        label1?.text = self.todos[indexPath.row]
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
+}
