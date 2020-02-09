cc0f7d8910e2f2d81ae0c92e3e7b211cc3b1f5ab
takeokunn <bararararatty@gmail.com>
2020-02-09 18:27:40 +0900

    Initial Commit

---
+++ TimeTableTodoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeTableTodoApp
+//
+//  Created by obara_t on 2020/02/09.
+//  Copyright © 2020 obara_t. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TimeTableTodoAppTests/TimeTableTodoAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  TimeTableTodoAppTests.swift
+//  TimeTableTodoAppTests
+//
+//  Created by obara_t on 2020/02/09.
+//  Copyright © 2020 obara_t. All rights reserved.
+//
+
+import XCTest
+@testable import TimeTableTodoApp
+
+class TimeTableTodoAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
